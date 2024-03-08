ce40a01d1006f741256482daf28104ca6a02cc7a
Zed <2044145178@qq.com>
2024-03-08 12:20:27 +0800

    Update config.ts
    补充注释信息

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const CONFIG={
-    username:"",
-    password:"",
+    username:"",//学号
+    password:"",//图书馆密码
     cancelLeftTime:300,//截止签到前取消时间（单位为s）
-    senderEmail: {
+    senderEmail: {//发送通知信息的邮箱
         // 发信人信息
         host: 'smtp.qq.com', // 发送邮箱服务器域名
         port: 465, // 端口
@@ -11,7 +11,7 @@ export const CONFIG={
         pass: '', // 发件人邮箱的授权码 这里可以通过邮箱获取 并且不唯一
         nickname: '', // 发信人昵称
     },
-    receiverEmail:'',
+    receiverEmail:'',//接受通知信息的邮箱
     pushdeerToken:'',//pushdeer APPtoken，不用留空即可
 }
 
